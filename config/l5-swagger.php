b99f48aa25fc975047dff84af0d4b103e671173d
David Silva <srdavidsilva@gmail.com>
2022-08-26 08:04:45 -0300

    prefix api

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -87,7 +87,7 @@ return [
             /*
              * Edit to set the api's base path
             */
-            'base' => env('L5_SWAGGER_BASE_PATH', null),
+            'base' => env('L5_SWAGGER_BASE_PATH', '/api/'),
 
             /*
              * Edit to set path where swagger ui assets should be stored
